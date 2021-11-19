b7a8ce0c73f8dd15c98d82e90df24ec5256e7e53
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2021-11-19 12:30:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,9 @@ define('CORE_DIR', ROOT . '/core');
 
 include_once(CORE_DIR . '/Game.php');
 include_once(CORE_DIR . '/Player.php');
+
+$game = new Game();
+$game->createPlayer('Leon');
+$game->setPlayerHp(100);
+$game->setPlayerDmg(50);
+$game->showPlayer();
