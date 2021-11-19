674091e79e0486771d6ce9e29320d0ced1bbd3eb
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2021-11-19 12:32:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
+<?php
+
 define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 define('CORE_DIR', ROOT . '/core');
 
+include_once(CORE_DIR . '/AbstractCharacter.php');
 include_once(CORE_DIR . '/Game.php');
 include_once(CORE_DIR . '/Player.php');
+include_once(CORE_DIR . '/Bot.php');
 
 $game = new Game();
 $game->createPlayer('Leon');
