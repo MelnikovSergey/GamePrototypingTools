6c20d2ddf108dbc72d2b9a34ed814263912fb0c6
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2021-11-20 14:42:16 +0300

    Update Player.php

--- core/Player.php
+++ core/Player.php
@@ -1,29 +1,10 @@
 <?php
 
-class Player
+class Player extends Abstract_Character
 {
-	private $_name;
-	private $_hp;
-	private $_dmg;
-
 	public function __construct(Game $game, $name)
 	{	
 		$this->_name = $name;
 		echo "Player {$this->_name} was created <br>";
-	}
-
-	public function setHp($int)
-	{	
-		$this->_hp = (int) $int;
-	}
-
-	public function setDmg($int)
-	{	
-		$this->_dmg = (int) $int;
-	}
-
-	public function __toString()
-	{	
-		return "Name: {$this->_name} | HP: {$this->_hp} | DMG: {$this->_dmg}";
 	}
 }
