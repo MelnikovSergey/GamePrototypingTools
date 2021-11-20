22cd01ccdfe5300e7411ce1fb54b44dfc8075f7c
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2021-11-20 14:42:36 +0300

    Update Bot.php

--- core/Bot.php
+++ core/Bot.php
@@ -1,1 +1,10 @@
 <?php
+
+class Bot extends Abstract_Character
+{
+	public function __construct(Game $game, $name)
+	{	
+		$this->_name = $name;
+		echo "Bot {$this->_name} was created <br>";
+	}
+}
