4cae48d676e2b70827adfb0a361c64b432a898e0
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2021-11-20 14:43:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ include_once(CORE_DIR . '/Player.php');
 include_once(CORE_DIR . '/Bot.php');
 
 $game = new Game();
-$game->createPlayer('Leon');
-$game->setPlayerHp(100);
-$game->setPlayerDmg(50);
-$game->showPlayer();
+$game->createCharacter('player', 'Leon');
+$game->createCharacter('bot', 'Boss');
+$game->createCharacter('fairy', 'the good fairy Sophia');
