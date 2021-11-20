fe3d2fa439723158fc8aa5d97acd632f4c2457b6
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2021-11-20 14:36:25 +0300

    Update AbstractCharacter.php

--- core/AbstractCharacter.php
+++ core/AbstractCharacter.php
@@ -1,1 +1,28 @@
 <?php
+
+abstract class Abstract_Character 
+{
+	protected $_name;
+	protected $_hp;
+	protected $_dmg;
+
+	public function setHp($int)
+	{	
+		$this->_hp = (int) $int;
+	}
+
+	public function setDmg($int)
+	{	
+		$this->_dmg = (int) $int;
+	}		
+
+	public function showCharacter()
+	{	
+		echo $this;
+	}
+
+	public function __toString()
+	{	
+		return "Player name: {$this->_name} | HP: {$this->_hp} | DMG: {$this->_dmg}<br>";
+	}
+}
