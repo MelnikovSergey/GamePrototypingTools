9fc59d0c109e4fe720725e342f0ffda47c2600d8
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2021-11-21 22:32:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,28 @@ define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 define('CORE_DIR', ROOT . '/core');
 
 include_once(CORE_DIR . '/AbstractCharacter.php');
+include_once(CORE_DIR . '/Session.php');
 include_once(CORE_DIR . '/Game.php');
 include_once(CORE_DIR . '/Player.php');
 include_once(CORE_DIR . '/Bot.php');
 
+$session = new Session();
 $game = new Game();
 
 $game->createCharacter('player', 'Leon');
-$game->player->setHp(100);
-$game->player->setDmg(50);
-$game->player->showCharacter();
+$game->player['Leon']->setHp(100);
+$game->player['Leon']->setDmg(50);
+$game->player['Leon']->showCharacter();
+
+$session->registerCharacter('Leon', $game->player['Leon']);
 
 $game->createCharacter('bot', 'Boss');
-$game->bot->setHp(80);
-$game->bot->setDmg(150);
-$game->bot->showCharacter();
+$game->bot['Boss']->setHp(80);
+$game->bot['Boss']->setDmg(150);
+$game->bot['Boss']->showCharacter();
 
-$game->createCharacter('fairy', 'the good fairy Sophia');
+$game->player['Leon']->battle($game->bot['Boss']);
+$game->player['Leon']->battle($game->bot['Boss']);
+
+$session->getCharacter('Leon');
+$session->showSession();
